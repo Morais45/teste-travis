38d157031213217f73a80fd616e49835886ed88b
joão victor <joaorob.m.l@gmail.com>
2026-03-09 19:51:13 -0300

    teste-travis

---
+++ soma.js
@@ -0,0 +1,10 @@
+function soma(a, b){
+    return a + b;
+}
+
+function soma2(a, b){
+    return a + b;
+}
+
+
+module.exports = soma;
---
+++ test/soma.test.js
@@ -0,0 +1,10 @@
+const soma = require('../soma');
+const soma2 = require('../soma');
+
+test('soma 1 + 2 e igual a 3', () => {
+    expect(soma(1, 2)).toBe(3);
+})
+
+test('soma 1 + 2 e igual a 3', () => {
+    expect(soma2(1, 2)).toBe(8);
+})

c1d1b5e9841fd1b339ca3ff6a26400650ac79c38
joão victor <joaorob.m.l@gmail.com>
2026-03-09 20:40:20 -0300

    Corrigir teste do soma2 para esperar 3

--- test/soma.test.js
+++ test/soma.test.js
@@ -5,6 +5,6 @@ test('soma 1 + 2 e igual a 3', () => {
     expect(soma(1, 2)).toBe(3);
 })
 
-test('soma 1 + 2 e igual a 3', () => {
-    expect(soma2(1, 2)).toBe(8);
+test('soma2 1 + 2 e igual a 3', () => {
+    expect(soma2(1, 2)).toBe(3);
 })

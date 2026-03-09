6ae9f44e5a145ce6455ea4d457454045b17707c6
joão victor <joaorob.m.l@gmail.com>
2026-03-09 20:39:29 -0300

    Atualização de testes e cobertura

--- soma.js
+++ soma.js
@@ -2,9 +2,5 @@ function soma(a, b){
     return a + b;
 }
 
-function soma2(a, b){
-    return a + b;
-}
-
 
 module.exports = soma;
--- test/soma.test.js
+++ test/soma.test.js
@@ -1,5 +1,5 @@
 const soma = require('../soma');
-const soma2 = require('../soma');
+const soma2 = require('../soma2');
 
 test('soma 1 + 2 e igual a 3', () => {
     expect(soma(1, 2)).toBe(3);
